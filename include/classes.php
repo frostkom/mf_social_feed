f083d5a83e29286f0275f062365f3ba8fa594d5d
Martin Fors <martin.fors@frostkom.se>
2025-04-14 11:32:37 +0200

    Don't force creating folder just to remove content if it exists

--- include/classes.php
+++ include/classes.php
@@ -108,10 +108,13 @@ class mf_social_feed
 
 			// Delete old uploads
 			#######################
-			list($upload_path, $upload_url) = get_uploads_folder($this->post_type);
+			list($upload_path, $upload_url) = get_uploads_folder($this->post_type, true, false);
 
-			get_file_info(array('path' => $upload_path, 'callback' => 'delete_files_callback', 'time_limit' => WEEK_IN_SECONDS));
-			get_file_info(array('path' => $upload_path, 'folder_callback' => 'delete_empty_folder_callback'));
+			if($upload_path != '')
+			{
+				get_file_info(array('path' => $upload_path, 'callback' => 'delete_files_callback', 'time_limit' => WEEK_IN_SECONDS));
+				get_file_info(array('path' => $upload_path, 'folder_callback' => 'delete_empty_folder_callback'));
+			}
 			#######################
 		}
 
